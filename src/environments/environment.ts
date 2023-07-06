ab2374c8411696803705368276b44dd333d95431
William Guillermo Guerrero Silva <william.guerrero@oxxofintech.com.mx>
2023-07-06 17:05:17 -0500

    Init project with firestore setup

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'easy-taste',
+    appId: '1:891893511875:web:691cea9b8eb847f9149476',
+    storageBucket: 'easy-taste.appspot.com',
+    apiKey: 'AIzaSyBbZlWagDk_4OdtfKTkkM-aLs3h1_FcLks',
+    authDomain: 'easy-taste.firebaseapp.com',
+    messagingSenderId: '891893511875',
+    measurementId: 'G-C3G1PGTVCY',
+  },
+};
